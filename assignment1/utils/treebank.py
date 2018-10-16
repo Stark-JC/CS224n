45669d8143b3d2e4dd40246c70fc9406dc04bebd
yejiacheng <401661801@qq.com>
2018-10-16 10:15:15 +0800

    add comment

--- assignment1/utils/treebank.py
+++ assignment1/utils/treebank.py
@@ -60,8 +60,8 @@ class StanfordSentiment:
 
                 splitted = line.strip().split()[1:]
                 # Deal with some peculiar encoding issues with this file
-                # sentences += [[w.lower().decode("utf-8").encode('latin1') for w in splitted]]
-                sentences += [[w.lower() for w in splitted]]
+                sentences += [[w.lower().encode('latin1') for w in splitted]]
+                # sentences += [[w.lower() for w in splitted]]
 
         self._sentences = sentences
         self._sentlengths = np.array([len(s) for s in sentences])
@@ -143,6 +143,7 @@ class StanfordSentiment:
         for i in range(self.numSentences()):
             sentence = sentences[i]
             full_sent = " ".join(sentence).replace('-lrb-', '(').replace('-rrb-', ')')
+            #print(full_sent)
             sent_labels[i] = labels[dictionary[full_sent]]
 
         self._sent_labels = sent_labels
